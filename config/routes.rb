3403643c7e9729e08feacace2ad3fe5291afbe34
tsubasa.katayama <tsubanetoon@yahoo.co.jp>
2021-02-18 06:59:47 +0000

    route-syuusei

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
   
-  resources :tasks, only: [:index, :show, :create, :destroy, :edit, :update]
+  resources :tasks, only: [:create, :destroy, :edit, :update]
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   
-   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 end
