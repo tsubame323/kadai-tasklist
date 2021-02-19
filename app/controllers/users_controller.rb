b12984623a6b156fafebeb80505700a353f74738
tsubasa.katayama <tsubanetoon@yahoo.co.jp>
2021-02-19 06:54:49 +0000

    taskkadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
 
   def new
     @user = User.new
