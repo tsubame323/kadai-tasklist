a883cc969c9093e7c16db9eed929f59ba3be16c0
tsubasa.katayama <tsubanetoon@yahoo.co.jp>
2021-02-19 04:41:00 +0000

    taskkadai

--- db/migrate/20210203060631_create_tasks.rb
+++ db/migrate/20210203060631_create_tasks.rb
@@ -3,7 +3,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :content
       t.string :status
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
